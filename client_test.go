825f44deaf870c9d36672a4f7eaa278ef032c4dd
Justice <justice@justicerobles.com>
2016-04-15 18:50:25 -0400

    Fix client test

--- client_test.go
+++ client_test.go
@@ -8,13 +8,8 @@ func TestSplit(t *testing.T) {
     t.Log("Starting Split Test")
     g := new(Goose)
     strs := g.Split("we are good", " ")
-    t.Log(strs[0])
-    var test []string = new []string{"we"}
-   // test[0] = "we"
-    //test[1] = "are"
-   // test[2] = "good"
-    t.Log(test)
-   if  test[0] == strs[0] {
+    test := g.Split("we don't like you"," ")
+    if  test[0] == strs[0] {
         t.Log("Correct answer")
     } else {
         t.Log("Arrays don't match ", strs, test)
