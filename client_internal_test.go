674738941daa9df7e3d451eb75ae4481ca0e5f33
Justice <justice@justicerobles.com>
2016-04-18 03:42:46 -0400

    Update client test

--- client_internal_test.go
+++ client_internal_test.go
@@ -10,8 +10,8 @@ func TestSplit(t *testing.T) {
     t.Log("Starting Split Test")
     g := new(goose.Goose)
     strs := g.Split("we are good", " ")
-    test := g.Split("we don't like you"," ")
-    if  test[0] == strs[0] {
+    var test []string = []string{"we","are","good"}
+    if  test[0] == strs[0] && test[1] == strs[1] {
         t.Log("Correct answer")
     } else {
         t.Log("Arrays don't match ", strs, test)
